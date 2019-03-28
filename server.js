ee8ea2c0147eedd50d9f3bd7721978dbdee32c0b
medcoguy <46691562+medcoguy@users.noreply.github.com>
2019-03-28 14:42:27 -0500

    final commit

--- server.js
+++ server.js
@@ -64,14 +64,20 @@ app.get("/", function(req, res) {
 });
 
 app.get("/scrape", function(req, res) {
+	// console.log("inside get route")
 	request("https://www.nytimes.com/section/world", function(error, response, html) {
-		var $ = cheerio.load(html);
+		
+	console.log(html)
+	// console.log(response)
+
+	var $ = cheerio.load(html);
 		var result = {};
-		$("div.story-body").each(function(i, element) {
+		$("div.css-1cp3ece").each(function(i, element) {
+			// console.log(element)
 			var link = $(element).find("a").attr("href");
-			var title = $(element).find("h2.headline").text().trim();
-			var summary = $(element).find("p.summary").text().trim();
-			var img = $(element).parent().find("figure.media").find("img").attr("src");
+			var title = $(element).find("h2").text().trim();
+			var summary = $(element).find("p").text().trim();
+			var img = $(element).parent().find("img").attr("src");
 			result.link = link;
 			result.title = title;
 			if (summary) {
@@ -83,11 +89,15 @@ app.get("/scrape", function(req, res) {
 			else {
 				result.img = $(element).find(".wide-thumb").find("img").attr("src");
 			};
-			var entry = new Article(result);
+			var entry = new Article(result); 
+			console.log ("entry", entry)
+
 			Article.find({title: result.title}, function(err, data) {
 				if (data.length === 0) {
 					entry.save(function(err, data) {
 						if (err) throw err;
+				console.log (data)
+
 					});
 				}
 			});
